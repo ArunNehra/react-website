1a51bda183bd34ba1fcb3bc2bda14dfab112921d
Arun.n <arun.n@taboola.com>
2021-07-22 19:03:45 +0530

    First commit

---
+++ src/app/components/navbar/navitems.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const ListContainer = styled.ul`
+    ${tw`flex list-none`}
+`;
+
+const NavItem = styled.li`
+    ${tw`
+        text-xs 
+        md:text-base
+        text-black
+    `}
+`;
+
+export function NavItems(){
+
+}
