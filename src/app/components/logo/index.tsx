c3d47344972df04a193e43c0a2b87a90cbf3f16c
Arun.n <arun.n@taboola.com>
2021-07-30 13:29:35 +0530

    Reusable Logo container, aboutUs section, topCars section, carousel_topcars and footer is also added successfully

--- src/app/components/logo/index.tsx
+++ src/app/components/logo/index.tsx
@@ -3,6 +3,12 @@ import styled from "styled-components";
 import tw from "twin.macro";
 
 import CarLogoImg from '../../../assets/images/car-logo.png';
+import CarLogoDarkImg from '../../../assets/images/car-logo-dark.png';
+
+interface ILogoProps {
+    color?: 'white' | 'dark';
+    bgColor?: 'white' | 'dark';
+}
 
 const LogoContainer = styled.div`
     ${tw`
@@ -19,6 +25,7 @@ const LogoText = styled.div`
         text-black
         m-1
     `}
+    ${({color}: any) => color ==='white' ? tw`text-white`: tw`text-black`}
 `;
 
 const Image = styled.div`
@@ -29,11 +36,13 @@ const Image = styled.div`
         height: 100%;
     }
 `;
-export function Logo() {
+export function Logo(props: ILogoProps) {
+    const {color, bgColor}  = props
+
     return <LogoContainer>
         <Image>
-            <img src={CarLogoImg} alt='logo' />
+            <img src={bgColor === 'dark' ? CarLogoDarkImg : CarLogoImg } alt='logo' />
         </Image>
-        <LogoText>CarBazaar.</LogoText>
+        <LogoText color={color || 'dark'}>CarBazaar.</LogoText>
     </LogoContainer>
 }
